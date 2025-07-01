3e8170648a653ed6e0d437fa0c9a722ad9f89c8f
Deiker <deiker@gmail.com>
2025-07-01 16:54:27 -0400

    Feat:Modelos de Pagos

--- gestion_pagos/models.py
+++ gestion_pagos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from clubes.models import ClubAtleta
 # Create your models here.
+
+class Pago_Mensualida(models.Model):
+    fecha_de_pago = models.DateField(auto_now_add=True)
+    atleta_club = models.ForeignKey(ClubAtleta, on_delete=models.CASCADE)
+    monto = models.FloatField()
+    mes_correspondiente = models.DateField()
+    foto_comprobante = models.ImageField(upload_to='comprobantes/', null=True, blank=True)
+    confirmacion = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'Pago_Mensualidades'
+
+class PagoInscripciones(models.Model):
+    fecha_de_pago = models.DateField(auto_now_add=True)
+    atleta_club = models.ForeignKey(ClubAtleta, on_delete=models.CASCADE)
+    monto = models.FloatField()
+    foto_comprobante = models.ImageField(upload_to='comprobantes/', null=True, blank=True)
+    confirmacion = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'Pago_Inscripciones'
